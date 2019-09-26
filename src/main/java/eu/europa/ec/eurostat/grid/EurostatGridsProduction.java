a4d951ae04a7724c472ed980d586be2296a214cd
jgaffuri <jgaf@protonmail.com>
2019-09-26 15:39:43 +0200

    class name change

--- src/main/java/eu/europa/ec/eurostat/grid/EurostatGridsProduction.java
+++ src/main/java/eu/europa/ec/eurostat/grid/EurostatGridsProduction.java
@@ -19,14 +19,14 @@ import eu.europa.ec.eurostat.grid.utils.SHPUtil;
  * @author julien Gaffuri
  *
  */
-public class Main {
-	static Logger logger = Logger.getLogger(Main.class.getName());
+public class EurostatGridsProduction {
+	static Logger logger = Logger.getLogger(EurostatGridsProduction.class.getName());
 
 	public static void main(String[] args) throws Exception {
 		logger.info("Start");
 
 		logger.setLevel(Level.ALL);
-		EuroGridBuilder.logger.setLevel(Level.ALL);
+		StatGridUtil.logger.setLevel(Level.ALL);
 
 		String path = "C:/Users/gaffuju/Desktop/grid/";
 		Geometry area = CountriesUtil.getEuropeMask();
@@ -35,7 +35,7 @@ public class Main {
 		//build pan-European grids
 		for(int res : new int[] {100,50,10,5}) {
 			logger.info("Make "+res+"km grid...");
-			Collection<Feature> cells = EuroGridBuilder.proceed(area, res*1000, 3035, "CNTR_ID", cnts, 1000, "CNTR_ID");
+			Collection<Feature> cells = StatGridUtil.proceed(area, res*1000, 3035, "CNTR_ID", cnts, 1000, "CNTR_ID");
 			logger.info("Save " + cells.size() + " cells...");
 			SHPUtil.saveSHP(cells, path+res+"km/grid_"+res+"km.shp", CRS.decode("EPSG:3035"));
 		}
@@ -43,12 +43,45 @@ public class Main {
 		//build country 1km grids by country
 		for(String countryCode : CountriesUtil.EuropeanCountryCodes) {
 			logger.info("Make 1km grid for "+countryCode+"...");
-			Collection<Feature> cells = EuroGridBuilder.buildGridCellsByCountry(countryCode, 1000, 500);
+			Collection<Feature> cells = buildGridCellsByCountry(countryCode, 1000, 500);
 			logger.info("Save " + cells.size() + " cells...");
 			SHPUtil.saveSHP(cells, path+"1km/grid_1km_"+countryCode+".shp", CRS.decode("EPSG:3035"));
 		}
 
 		logger.info("End");
+	}
+
+
+
+
+	/**
+	 * Build grid covering a single country.
+	 * In EPSG 3035 only.
+	 * 
+	 * @param countryCode
+	 * @param gridResolutionM
+	 * @param toleranceDistance
+	 * @return
+	 */
+	public static Collection<Feature> buildGridCellsByCountry(String countryCode, double gridResolutionM, double toleranceDistance) {
+
+		//get country geometry
+		Geometry cntGeom = CountriesUtil.getCountry(countryCode).getDefaultGeometry();
+		if(toleranceDistance >= 0)
+			cntGeom = cntGeom.buffer(toleranceDistance);
+
+		//build cells
+		StatGrid grid = new StatGrid()
+				.setGeometryToCover(cntGeom)
+				.setResolution(gridResolutionM)
+				.setEPSGCode(3035)
+				;
+		Collection<Feature> cells = grid.getCells();
+
+		//set cnt code to cells
+		for(Feature cell : cells) cell.setAttribute("CNTR_ID", countryCode);
+
+		return cells;
 	}
 
 }
