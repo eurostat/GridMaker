884a76e4803d69f76ec81a4131240201913d3357
jgaffuri <jgaf@protonmail.com>
2019-09-27 17:32:14 +0200

    geojson and shp support

--- src/main/java/eu/europa/ec/eurostat/grid/GridMakerJarMain.java
+++ src/main/java/eu/europa/ec/eurostat/grid/GridMakerJarMain.java
@@ -4,6 +4,7 @@
 package eu.europa.ec.eurostat.grid;
 
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.Collection;
 
 import org.apache.commons.cli.CommandLine;
@@ -14,12 +15,12 @@ import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
 import org.geotools.referencing.CRS;
 import org.locationtech.jts.geom.Geometry;
-import org.opengis.referencing.FactoryException;
 import org.opengis.referencing.crs.CoordinateReferenceSystem;
 
 import eu.europa.ec.eurostat.grid.StatGrid.GridCellGeometryType;
 import eu.europa.ec.eurostat.grid.utils.CountriesUtil;
 import eu.europa.ec.eurostat.grid.utils.Feature;
+import eu.europa.ec.eurostat.grid.utils.GeoJSONUtil;
 import eu.europa.ec.eurostat.grid.utils.SHPUtil;
 import eu.europa.ec.eurostat.grid.utils.SHPUtil.SHPData;
 
@@ -32,7 +33,7 @@ import eu.europa.ec.eurostat.grid.utils.SHPUtil.SHPData;
  */
 public class GridMakerJarMain {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 
 		Options options = new Options();
 		options.addOption(Option.builder("res").longOpt("resolution").desc("Optional. The grid resolution (pixel size). Note that the unit of measure is expected to be the same as the one of the coordinate reference system. Default: '100 000'.")
@@ -43,15 +44,21 @@ public class GridMakerJarMain {
 		options.addOption(Option.builder("cnt").longOpt("countryCode").desc("The 2 letters code of the country to build the grid accross.")
 				.hasArg().argName("code").build());
 		//TODO envelope
-		options.addOption(Option.builder("i").longOpt("inputFile").desc("Input file (SHP format) containing the geometry of the region to build the grid accross.")
+		options.addOption(Option.builder("if").longOpt("inputFileFormat").desc("Optional. Input file format. Default: 'geojson'.")
+				.hasArg().argName("shp or geojson").build());
+		options.addOption(Option.builder("i").longOpt("inputFile").desc("Input file containing the geometry of the region to build the grid accross.")
 				.hasArg().argName("file path").build());
 
 		options.addOption(Option.builder("tol").longOpt("toleranceDistance").desc("Optional. A tolerance distance to keep the cells that are not too far from the specified region. Default: '0'.")
 				.hasArg().argName("value").build());
 		options.addOption(Option.builder("gt").longOpt("gridCellGeometryType").desc("Optional. The type of grid cell geometry: The surface representation (a square) or its center point. Default: 'SURFACE'.")
 				.hasArg().argName("SURFACE or CENTER_POINT").build());
+
+		options.addOption(Option.builder("of").longOpt("outputFileFormat").desc("Optional. Output file format. Default: 'geojson'.")
+				.hasArg().argName("shp or geojson").build());
 		options.addOption(Option.builder("o").longOpt("outputFile").desc("Optional. Output file (SHP format). Default: 'out.shp'.")
 				.hasArg().argName("file path").build());
+
 		options.addOption(Option.builder("h").desc("Show this help message").build());
 
 		CommandLine cmd = null;
@@ -77,9 +84,11 @@ public class GridMakerJarMain {
 		} catch (Exception e) { System.err.println("Failed reading parameter 'res'. The default value will be used."); }
 
 		//epsg
+		CoordinateReferenceSystem crs = null;
 		param = cmd.getOptionValue("epsg");
 		try { if(param != null) sg.setEPSGCode(param);
 		} catch (Exception e) { System.err.println("Failed reading parameter 'epsg'. The default value will be used."); }
+		crs = CRS.parseWKT(WKT_3035);
 
 		//cnt
 		param = cmd.getOptionValue("cnt");
@@ -90,16 +99,37 @@ public class GridMakerJarMain {
 		}
 		} catch (Exception e) { System.err.println("Failed finding country with parameter 'cnt': " + param); }
 
+		//if
+		String inputFileFormat = cmd.getOptionValue("if");
+		if(inputFileFormat == null)
+			inputFileFormat = "geojson";
+		else if(!"geojson".equals(inputFileFormat) && !"shp".equals(inputFileFormat)) {
+			System.out.println("Unexpected input file format: "+inputFileFormat+". 'geojson' will be used.");
+			inputFileFormat = "geojson";
+		}
+
 		//i
 		param = cmd.getOptionValue("i");
 		try { if(param != null) {
-			SHPData sh = SHPUtil.loadSHP(param);
-			if(sh.fs.size() == 0)
+
+			ArrayList<Feature> fs = null;
+			if("shp".equals(inputFileFormat)) {
+				SHPData sh = SHPUtil.loadSHP(param);
+				fs = sh.fs;
+				crs = sh.ft.getCoordinateReferenceSystem();
+				System.out.println(crs);
+				System.out.println(SHPUtil.getCRS(param));
+			} else {
+				fs = GeoJSONUtil.load(param);
+				crs = GeoJSONUtil.loadCRS(param);
+			}
+
+			if(fs.size() == 0)
 				System.err.println("Input file is empty: " + param);
 			else {
-				if(sh.fs.size() > 1)
-					System.err.println("Input file contains more than one geometry (nb="+sh.fs.size()+"): " + param);
-				Geometry geomToCover = sh.fs.iterator().next().getDefaultGeometry();
+				if(fs.size() > 1)
+					System.err.println("Input file contains more than one geometry (nb="+fs.size()+"): " + param);
+				Geometry geomToCover = fs.iterator().next().getDefaultGeometry();
 				sg.setGeometryToCover(geomToCover);
 			}
 		}
@@ -119,9 +149,18 @@ public class GridMakerJarMain {
 		}
 		} catch (Exception e) { System.err.println("Failed reading parameter 'gt'. The default value will be used."); }
 
+		//of
+		String outputFileFormat = cmd.getOptionValue("of");
+		if(outputFileFormat == null)
+			outputFileFormat = "geojson";
+		else if(!"geojson".equals(outputFileFormat) && !"shp".equals(outputFileFormat)) {
+			System.out.println("Unexpected output file format: "+outputFileFormat+". 'geojson' will be used.");
+			outputFileFormat = "geojson";
+		}
+
 		//output file
 		String outFile = cmd.getOptionValue("o");
-		if(outFile == null) outFile = Paths.get("").toAbsolutePath().toString() + "/out.shp";
+		if(outFile == null) outFile = Paths.get("").toAbsolutePath().toString() + "/out."+outputFileFormat;
 
 
 		System.out.println("Build grid...");
@@ -129,19 +168,13 @@ public class GridMakerJarMain {
 		System.out.println(cells.size() + " grid cells built.");
 
 		System.out.println("Save as " + outFile + "...");
-		try {
-			//TODO fix that !
-			CoordinateReferenceSystem crs = null;
-			if("3035".equals(sg.getEPSGCode()))
-				crs = CRS.parseWKT(WKT_3035);
-			else
-				crs = CRS.decode("EPSG:"+sg.getEPSGCode());
-
+		//save
+		if("shp".equals(inputFileFormat))
 			SHPUtil.saveSHP(cells, outFile, crs);
-		} catch (FactoryException e) { e.printStackTrace(); }
+		else
+			GeoJSONUtil.save(cells, outFile, crs);
 
 	}
-
 
 
 	private static final String WKT_3035 = "PROJCS[\"ETRS89 / ETRS-LAEA\",\r\n" + 
---
+++ src/main/java/eu/europa/ec/eurostat/grid/utils/GeoJSONUtil.java
@@ -0,0 +1,107 @@
+/**
+ * 
+ */
+package eu.europa.ec.eurostat.grid.utils;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.StringWriter;
+import java.io.Writer;
+import java.util.ArrayList;
+import java.util.Collection;
+
+import org.geotools.data.simple.SimpleFeatureCollection;
+import org.geotools.feature.FeatureCollection;
+import org.geotools.geojson.feature.FeatureJSON;
+import org.geotools.geojson.geom.GeometryJSON;
+import org.locationtech.jts.geom.Geometry;
+import org.opengis.referencing.crs.CoordinateReferenceSystem;
+
+/**
+ * @author julien Gaffuri
+ *
+ */
+public class GeoJSONUtil {
+
+	public static SimpleFeatureCollection loadFC(String filePath) {
+		try {
+			InputStream input = new FileInputStream(new File(filePath));
+			FeatureCollection<?,?> fc = new FeatureJSON().readFeatureCollection(input);
+			input.close();
+			return (SimpleFeatureCollection)fc;
+		} catch (Exception e) { e.printStackTrace(); }
+		return null;
+	}
+
+	public static CoordinateReferenceSystem loadCRS(String filePath) {
+		try {
+			InputStream input = new FileInputStream(new File(filePath));
+			CoordinateReferenceSystem crs = new FeatureJSON().readCRS(input);
+			input.close();
+			return crs;
+		} catch (Exception e) { e.printStackTrace(); }
+		return null;
+	}
+
+	public static ArrayList<Feature>  load(String filePath) {
+		SimpleFeatureCollection sfc = loadFC(filePath);
+		if(sfc == null) return null;
+		return SimpleFeatureUtil.get(sfc);
+	}
+
+	public static void save(Collection<? extends Feature> fs, String outFile, CoordinateReferenceSystem crs) { save(SimpleFeatureUtil.get(fs, crs), outFile); }
+	public static void save(FeatureCollection<?,?> fc, String outFile) {
+		try {
+			OutputStream output = new FileOutputStream(new File(outFile));
+			new FeatureJSON().writeFeatureCollection(fc, output);
+			output.close();
+		} catch (Exception e) { e.printStackTrace(); }
+	}
+
+
+
+
+	public static String toGeoJSON(Geometry geom){
+		String out = null;
+		try {
+			StringWriter writer = new StringWriter();
+			new GeometryJSON().write(geom, writer);
+			out = writer.toString();
+			writer.close();
+		} catch (IOException e) { e.printStackTrace();
+		} finally {
+		}
+		return out;
+	}
+
+
+
+	/**
+	 * Convert a SHP file into a geoJSON file
+	 * 
+	 * @param inSHPFilePath
+	 * @param outGeoJSONFilePath
+	 */
+	public static void toGeoJSON(Collection<Feature> fs, String outPath, String outFile) { toGeoJSON(SimpleFeatureUtil.get(fs,null), outPath, outFile); }
+	public static void toGeoJSON(Collection<Feature> fs, Writer writer) { toGeoJSON(SimpleFeatureUtil.get(fs,null), writer); }
+	public static void toGeoJSON(SimpleFeatureCollection fc, String outPath, String outFile) {
+		try {
+			new File(outPath).mkdirs();
+			FileWriter fw = new FileWriter(outPath + outFile);
+			toGeoJSON(fc, fw);
+			fw.close();
+		} catch (IOException e) { e.printStackTrace(); }
+	}
+	public static void toGeoJSON(SimpleFeatureCollection fc, Writer writer) {
+		try {
+			new FeatureJSON().writeFeatureCollection(fc, writer);
+		} catch (IOException e) { e.printStackTrace(); }
+	}
+	public static void toGeoJSON(String inSHPFilePath, String outPath, String outFile) { toGeoJSON(SHPUtil.getSimpleFeatures(inSHPFilePath), outPath, outFile); }
+
+}
