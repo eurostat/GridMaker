e2e3f10ce5936aa6669f05999e8c3403c9350457
jgaffuri <jgaf@protonmail.com>
2019-10-02 10:58:53 +0200

    use logger

--- src/main/java/eu/europa/ec/eurostat/grid/examples/CntPreparation.java
+++ src/main/java/eu/europa/ec/eurostat/grid/examples/CntPreparation.java
@@ -3,6 +3,7 @@ package eu.europa.ec.eurostat.grid.examples;
 import java.util.ArrayList;
 import java.util.Collection;
 
+import org.apache.log4j.Logger;
 import org.geotools.referencing.CRS;
 import org.locationtech.jts.geom.Geometry;
 import org.opengis.referencing.crs.CoordinateReferenceSystem;
@@ -12,34 +13,37 @@ import eu.europa.ec.eurostat.grid.utils.Feature;
 import eu.europa.ec.eurostat.grid.utils.SHPUtil;
 
 public class CntPreparation {
+	static Logger logger = Logger.getLogger(CntPreparation.class.getName());
 
 	public static void main(String[] args) throws Exception {
-		System.out.println("Start");
+		logger.info("Start");
 
 		String path = "C:\\Users\\gaffuju\\Desktop\\CNTR_100k\\";
 		CoordinateReferenceSystem crs = CRS.decode("EPSG:3035");
 
 		String[] versions = new String[] {"2004","2010","2013","2016"};
 
+
+
 		//produce country union
 		Collection<Feature> cnts = new ArrayList<>();
 		for(String cntC : CountriesUtil.EuropeanCountryCodes) {
-			System.out.println(cntC);
+			logger.info(cntC);
 			Geometry cntGeom = null;
 			for(String version : versions) {
-				System.out.println(version);
+				logger.info(version);
 				Geometry cntGeomV = null;
 				try {
 					cntGeomV = CountriesUtil.getEuropeanCountry(cntC, path+"CNTR_RG_100K_"+version+"_LAEA.shp").getDefaultGeometry();
 					cntGeomV = cntGeomV.buffer(0);
 				} catch (Exception e) {
-					System.out.println(e.getMessage());
+					logger.info(e.getMessage());
 					continue;
 				}
 				if(cntGeom == null)
 					cntGeom = cntGeomV;
 				else
-					cntGeomV = cntGeom.union(cntGeomV);
+					cntGeom = cntGeom.union(cntGeomV);
 			}
 			Feature cnt = new Feature();
 			cnt.setAttribute("CNTR_ID", cntC);
@@ -48,10 +52,14 @@ public class CntPreparation {
 		}
 		SHPUtil.saveSHP(cnts, path+"CNTR_RG_100K_union_LAEA.shp", crs);
 
-		//produce europe 100k
+
+
+
+		logger.info("Produce europe 100k from 100k countries");
 		SHPUtil.union(path+"CNTR_RG_100K_union_LAEA.shp", path+"Europe_100K_union_LAEA.shp");
 
-		System.out.println("End");
+
+		logger.info("End");
 	}
 
 }
