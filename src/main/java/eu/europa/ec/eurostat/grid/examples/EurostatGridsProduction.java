a1928b14629dc4ce4a7b080864e0e9b57e45e9d2
jgaffuri <jgaf@protonmail.com>
2019-10-04 14:07:15 +0200

    new process based on prepared (buffered) data

--- src/main/java/eu/europa/ec/eurostat/grid/examples/EurostatGridsProduction.java
+++ src/main/java/eu/europa/ec/eurostat/grid/examples/EurostatGridsProduction.java
@@ -8,6 +8,7 @@ import java.util.Collection;
 
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
+import org.geotools.filter.text.cql2.CQL;
 import org.geotools.referencing.CRS;
 import org.locationtech.jts.geom.Geometry;
 import org.opengis.referencing.crs.CoordinateReferenceSystem;
@@ -40,20 +41,21 @@ public class EurostatGridsProduction {
 		StatGridCountryUtil.logger.setLevel(Level.ALL);
 
 		String outpath = "C:/Users/gaffuju/Desktop/grid/";
-		String path = "C:\\Users\\gaffuju\\Desktop\\CNTR_100k\\";
+		String path = "C:/Users/gaffuju/Desktop/CNTR_100k/";
 		CoordinateReferenceSystem crs = CRS.decode("EPSG:3035");
+		int bufferDistance = 2000; //TODO change to 1500 only?
 
 		//make pan-european grid datasets
 		logger.info("Get Europe cover (buffer)...");
-		Geometry europeCover = SHPUtil.loadSHP(path+"Europe_100K_union_buff_2000_LAEA.shp").fs.iterator().next().getDefaultGeometry();
+		Geometry europeCover = SHPUtil.loadSHP(path+"Europe_100K_union_buff_"+bufferDistance+"_LAEA.shp").fs.iterator().next().getDefaultGeometry();
 
 		logger.info("Get European countries ...");
-		ArrayList<Feature> cnts = SHPUtil.loadSHP(path+"CNTR_RG_100K_union_buff_2000_LAEA.shp").fs;
+		ArrayList<Feature> cnts = SHPUtil.loadSHP(path+"CNTR_RG_100K_union_buff_"+bufferDistance+"_LAEA.shp").fs;
 
 		//build pan-European grids for various resolutions
 		for(int resKM : new int[] {100,50,20,10,5}) {
 			logger.info("Make " + resKM + "km grid...");
-			make(resKM, europeCover, cnts, outpath, crs);
+			make(resKM, europeCover, cnts, bufferDistance, outpath, crs);
 		}
 
 
@@ -62,7 +64,21 @@ public class EurostatGridsProduction {
 		for(String countryCode : CountriesUtil.EuropeanCountryCodes) {
 
 			logger.info("Make 1km grid for "+countryCode+"...");
-			Collection<Feature> cells = buildGridCellsByCountry(countryCode, true, 1000, 1000);
+
+			//get country geometry (buffer)
+			Geometry countryGeom = SHPUtil.loadSHP(path+"CNTR_RG_100K_union_buff_"+bufferDistance+"_LAEA.shp", CQL.toFilter("CNTR_ID = '"+countryCode+"'"))
+					.fs.iterator().next().getDefaultGeometry();
+
+			//build cells
+			StatGrid grid = new StatGrid()
+					.setResolution(1000)
+					.setEPSGCode("3035")
+					.setGeometryToCover(countryGeom)
+					;
+			Collection<Feature> cells = grid.getCells();
+
+			//set country code to cells
+			for(Feature cell : cells) cell.setAttribute("CNTR_ID", countryCode);
 
 			logger.info("Save " + cells.size() + " cells...");
 			SHPUtil.saveSHP(cells, outpath+"1km/grid_1km_"+countryCode+".shp", crs);
@@ -75,7 +91,7 @@ public class EurostatGridsProduction {
 		//make(2, europeCover, cnts, path, crs);
 		//try to make 1km for whole Europe
 		logger.info("Make " + 1 + "km grid...");
-		make(1, europeCover, cnts, outpath, crs);
+		make(1, europeCover, cnts, bufferDistance, outpath, crs);
 
 		logger.info("End");
 	}
@@ -83,51 +99,19 @@ public class EurostatGridsProduction {
 
 
 
-	private static void make(int resKM, Geometry europeCover, ArrayList<Feature> cnts, String path, CoordinateReferenceSystem crs) {
+	private static void make(int resKM, Geometry europeCover, ArrayList<Feature> cnts, double bufferDistance, String path, CoordinateReferenceSystem crs) {
 		StatGrid grid = new StatGrid()
 				.setResolution(resKM*1000.0)
 				.setEPSGCode("3035")
 				.setGeometryToCover(europeCover)
 				;
 		Collection<Feature> cells = grid.getCells();
-		StatGridCountryUtil.assignCountries(cells, "CNTR_ID", cnts, 2000, "CNTR_ID");
+		StatGridCountryUtil.assignCountries(cells, "CNTR_ID", cnts, bufferDistance, "CNTR_ID");
 		StatGridCountryUtil.filterCellsWithoutCountry(cells, "CNTR_ID");
 
 		logger.info("Save " + cells.size() + " cells...");
 		SHPUtil.saveSHP(cells, path+resKM+"km/grid_"+resKM+"km.shp", crs);
 	}
 
-
-
-
-	/**
-	 * Build grid covering a single country.
-	 * In EPSG 3035 only.
-	 * 
-	 * @param countryCode
-	 * @param gridResolutionM
-	 * @param toleranceDistance
-	 * @return
-	 */
-	public static Collection<Feature> buildGridCellsByCountry(String countryCode, boolean withOST, double gridResolutionM, double toleranceDistance) {
-
-		//get country geometry
-		//TODO get bufferred geometry
-		Geometry cntGeom = CountriesUtil.getEuropeanCountry(countryCode, withOST).getDefaultGeometry();
-
-		//build cells
-		StatGrid grid = new StatGrid()
-				.setResolution(gridResolutionM)
-				.setEPSGCode("3035")
-				.setGeometryToCover(cntGeom)
-				.setToleranceDistance(toleranceDistance)
-				;
-		Collection<Feature> cells = grid.getCells();
-
-		//set country code to cells
-		for(Feature cell : cells) cell.setAttribute("CNTR_ID", countryCode);
-
-		return cells;
-	}
 
 }
