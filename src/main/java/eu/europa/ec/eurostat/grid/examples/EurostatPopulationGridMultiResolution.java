d6804941a07e5ac4b849efce9db9f6827ed692ee
jgaffuri <jgaf@protonmail.com>
2019-10-07 13:29:17 +0200

    preparation

---
+++ src/main/java/eu/europa/ec/eurostat/grid/examples/EurostatPopulationGridMultiResolution.java
@@ -0,0 +1,52 @@
+/**
+ * 
+ */
+package eu.europa.ec.eurostat.grid.examples;
+
+import org.apache.log4j.Logger;
+import org.locationtech.jts.geom.Envelope;
+import org.locationtech.jts.geom.Polygon;
+
+/**
+ * 
+ * Derive EU population figures from 1km grid to other larger resolutions.
+ * 
+ * @author julien Gaffuri
+ *
+ */
+public class EurostatPopulationGridMultiResolution {
+	static Logger logger = Logger.getLogger(EurostatPopulationGridMultiResolution.class.getName());
+
+	public static void main(String[] args) {
+		logger.info("Start");
+
+		//load 1km population
+
+		for(int resKM : new int[] {100,50,20,10,5}) {
+			//TODO
+			//go through 1km population data
+			//get higher resolution grid cell it belong to
+			//increment population value
+
+			//save
+		}
+
+		logger.info("End");
+	}
+
+
+	public static boolean contains(String gridCellId1, String gridCellId2) {
+		//TODO
+		return false;
+	}
+
+	public static Polygon getGeometry(String cellCode) {
+		//TODO
+		return null;
+	}
+
+	public static Envelope getEnvelope(String cellCode) {
+		//TODO
+		return null;
+	}
+}
