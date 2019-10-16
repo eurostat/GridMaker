7dd0358bd312b35ec2c32b2a73a80a20183dfefb
jgaffuri <jgaf@protonmail.com>
2019-10-16 22:18:16 +0200

    population grid

--- src/main/java/eu/europa/ec/eurostat/grid/examples/EurostatPopulationGridMultiResolution.java
+++ src/main/java/eu/europa/ec/eurostat/grid/examples/EurostatPopulationGridMultiResolution.java
@@ -23,7 +23,7 @@ public class EurostatPopulationGridMultiResolution {
 
 		String basePath = "C:/Users/gaffuju/Desktop/";
 
-		//****: GRD_ID;TOT_P;  YEAR;CNTR_CODE;METHD_CL;DATA_SRC
+		//GRD_ID; TOT_P; YEAR; CNTR_CODE; METHD_CL; DATA_SRC
 		//grid cell example: 1kmN2689E4341
 
 		for(int year : new int[] { 2006, 2011 }) {
@@ -32,9 +32,20 @@ public class EurostatPopulationGridMultiResolution {
 			//load 1km data
 			StatsHypercube popData = CSV.load(basePath+"pop_grid_1km/"+year+".csv", "TOT_P");
 			popData.printInfo(false);
+			//TODO remove unnecessary dimensions
+			popData.delete("YEAR");
+			popData.delete("CNTR_CODE");
+			popData.delete("METHD_CL");
+			popData.delete("DATA_SRC");
+			popData.printInfo(false);
+
+			//TODO reformat grid cell
+			//CSV.save(popData, "TOT_P", basePath+"pop_grid/", "pop_grid_"+year+"_1km.csv");
 
 			for(int resKM : EurostatGridsProduction.resKMs) {
 
+				//output data
+				StatsHypercube sh = new StatsHypercube("GRD_ID");
 
 				//TODO
 				//go through 1km population data
@@ -42,11 +53,9 @@ public class EurostatPopulationGridMultiResolution {
 				//increment population value
 
 				//save
+				CSV.save(sh, "TOT_P", basePath+"pop_grid/", "pop_grid_"+year+"_"+resKM+"km.csv");
 			}
-
 		}
-
-
 		logger.info("End");
 	}
 
