27254b14f433ba068f3853c8ab7f55e3e06aad6d
jgaffuri <jgaf@protonmail.com>
2019-10-16 18:28:33 +0200

    multi resolution population

--- src/main/java/eu/europa/ec/eurostat/grid/examples/EurostatPopulationGridMultiResolution.java
+++ src/main/java/eu/europa/ec/eurostat/grid/examples/EurostatPopulationGridMultiResolution.java
@@ -5,6 +5,9 @@ package eu.europa.ec.eurostat.grid.examples;
 
 import org.apache.log4j.Logger;
 
+import eu.europa.ec.eurostat.java4eurostat.base.StatsHypercube;
+import eu.europa.ec.eurostat.java4eurostat.io.CSV;
+
 /**
  * 
  * Derive EU population figures from 1km grid to other larger resolutions.
@@ -18,16 +21,31 @@ public class EurostatPopulationGridMultiResolution {
 	public static void main(String[] args) {
 		logger.info("Start");
 
-		//load 1km population
+		String basePath = "C:/Users/gaffuju/Desktop/";
 
-		for(int resKM : EurostatGridsProduction.resKMs) {
-			//TODO
-			//go through 1km population data
-			//get higher resolution grid cell it belong to
-			//increment population value
+		//****: GRD_ID;TOT_P;  YEAR;CNTR_CODE;METHD_CL;DATA_SRC
+		//grid cell example: 1kmN2689E4341
 
-			//save
+		for(int year : new int[] { 2006, 2011 }) {
+			logger.info(year);
+
+			//load 1km data
+			StatsHypercube popData = CSV.load(basePath+"pop_grid_1km/"+year+".csv", "TOT_P");
+			popData.printInfo(false);
+
+			for(int resKM : EurostatGridsProduction.resKMs) {
+
+
+				//TODO
+				//go through 1km population data
+				//get higher resolution grid cell it belong to
+				//increment population value
+
+				//save
+			}
+
 		}
+
 
 		logger.info("End");
 	}
