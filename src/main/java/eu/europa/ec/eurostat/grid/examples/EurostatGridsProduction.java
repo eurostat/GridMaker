9a6be05f885aadb1bafa6ea79312d22693cb53ee
jgaffuri <jgaf@protonmail.com>
2019-10-16 16:34:32 +0200

    handle gpkg format

--- src/main/java/eu/europa/ec/eurostat/grid/examples/EurostatGridsProduction.java
+++ src/main/java/eu/europa/ec/eurostat/grid/examples/EurostatGridsProduction.java
@@ -8,14 +8,13 @@ import java.util.Collection;
 
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
-import org.geotools.filter.text.cql2.CQL;
 import org.geotools.referencing.CRS;
 import org.locationtech.jts.geom.Geometry;
 import org.opengis.referencing.crs.CoordinateReferenceSystem;
 
 import eu.europa.ec.eurostat.grid.StatGrid;
-import eu.europa.ec.eurostat.jgiscotools.CountriesUtil;
 import eu.europa.ec.eurostat.jgiscotools.datamodel.Feature;
+import eu.europa.ec.eurostat.jgiscotools.io.GeoPackageUtil;
 import eu.europa.ec.eurostat.jgiscotools.io.SHPUtil;
 
 /**
@@ -30,7 +29,6 @@ public class EurostatGridsProduction {
 
 	static int[] resKMs = new int[] {100,50,20,10,5};
 
-	//TODO save as geopkg ?
 	//see also:
 	//https://www.eea.europa.eu/data-and-maps/data/eea-reference-grids-2
 	//https://www.efgs.info/data/
@@ -58,12 +56,12 @@ public class EurostatGridsProduction {
 		//build pan-European grids for various resolutions
 		for(int resKM : resKMs) {
 			logger.info("Make " + resKM + "km grid...");
-			make(resKM, europeCover, cnts, bufferDistance, outpath, crs);
+			make(resKM, europeCover, cnts, bufferDistance, outpath, crs, true);
 		}
 
 
 
-		//build country 1km grids by country
+		/*/build country 1km grids by country
 		for(String countryCode : CountriesUtil.EuropeanCountryCodes) {
 
 			logger.info("Make 1km grid for "+countryCode+"...");
@@ -83,16 +81,19 @@ public class EurostatGridsProduction {
 			//set country code to cells
 			for(Feature cell : cells) cell.setAttribute("CNTR_ID", countryCode);
 
-			logger.info("Save " + cells.size() + " cells...");
+			logger.info("Save " + cells.size() + " cells as SHP...");
 			SHPUtil.saveSHP(cells, outpath+"1km/grid_1km_"+countryCode+".shp", crs);
+			logger.info("Save " + cells.size() + " cells as GPKG...");
+			GeoPackageUtil.save(cells, outpath+"1km/grid_1km_"+countryCode+".gpkg", crs);
 		}
+		 */
 
-		//try to make 2km for whole Europe
-		//logger.info("Make " + 2 + "km grid...");
-		//make(2, europeCover, cnts, path, crs);
-		//try to make 1km for whole Europe
-		//logger.info("Make " + 1 + "km grid...");
-		//make(1, europeCover, cnts, bufferDistance, outpath, crs);
+		//2km for whole Europe
+		logger.info("Make " + 2 + "km grid...");
+		make(2, europeCover, cnts, bufferDistance, outpath, crs, false);
+		//1km for whole Europe
+		logger.info("Make " + 1 + "km grid...");
+		make(1, europeCover, cnts, bufferDistance, outpath, crs, false);
 
 		logger.info("End");
 	}
@@ -100,18 +101,26 @@ public class EurostatGridsProduction {
 
 
 
-	private static void make(int resKM, Geometry europeCover, ArrayList<Feature> cnts, double bufferDistance, String path, CoordinateReferenceSystem crs) {
+	private static void make(int resKM, Geometry europeCover, ArrayList<Feature> cnts, double bufferDistance, String path, CoordinateReferenceSystem crs, boolean saveSPH) {
+		//build grid
 		StatGrid grid = new StatGrid()
 				.setResolution(resKM*1000)
 				.setEPSGCode("3035")
 				.setGeometryToCover(europeCover)
 				;
 		Collection<Feature> cells = grid.getCells();
+
 		StatGridCountryUtil.assignCountries(cells, "CNTR_ID", cnts, bufferDistance, "CNTR_ID");
 		//TODO assign also nuts code
 		StatGridCountryUtil.filterCellsWithoutCountry(cells, "CNTR_ID");
-		logger.info("Save " + cells.size() + " cells...");
-		SHPUtil.saveSHP(cells, path+resKM+"km/grid_"+resKM+"km.shp", crs);
+
+		//save
+		/*if(saveSPH) {
+			logger.info("Save " + cells.size() + " cells as SHP...");
+			SHPUtil.saveSHP(cells, path+resKM+"km/grid_"+resKM+"km.shp", crs);
+		}*/
+		logger.info("Save " + cells.size() + " cells as GPKG...");
+		GeoPackageUtil.save(cells, path+resKM+"km/grid_"+resKM+"km.gpkg", crs);
 	}
-	
+
 }
