bb739d5ac17f3a6e89c87e32acda8c4732d3f6d2
jgaffuri <jgaf@protonmail.com>
2019-11-06 19:04:56 +0100

    a

--- src/main/java/eu/europa/ec/eurostat/grid/examples/EurostatGridsProduction.java
+++ src/main/java/eu/europa/ec/eurostat/grid/examples/EurostatGridsProduction.java
@@ -9,9 +9,6 @@ import java.util.Collection;
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 import org.geotools.referencing.CRS;
-import org.locationtech.jts.geom.Geometry;
-import org.locationtech.jts.index.SpatialIndex;
-import org.locationtech.jts.index.strtree.STRtree;
 import org.opengis.referencing.crs.CoordinateReferenceSystem;
 
 import eu.europa.ec.eurostat.grid.Grid;
@@ -53,12 +50,14 @@ public class EurostatGridsProduction {
 		logger.info("Get European countries (buffer) ...");
 		ArrayList<Feature> cntsBuff = SHPUtil.loadSHP(path+"CNTR_RG_100K_union_buff_"+bufferDistance+"_LAEA.shp").fs;
 
+		/*
 		logger.info("Get land area...");
 		Collection<Geometry> landGeometries = FeatureUtil.getGeometriesSimple( SHPUtil.loadSHP(path+"CNTR_RG_100K_union_LAEA.shp").fs );
 		//TODO tile land areas
 		SpatialIndex landGeometriesIndex = new STRtree();
 		for(Geometry g : landGeometries) landGeometriesIndex.insert(g.getEnvelopeInternal(), g);
 		landGeometries = null;
+		 */
 
 		//build pan-European grids
 		for(int resKM : resKMs) {
@@ -81,8 +80,9 @@ public class EurostatGridsProduction {
 
 			//TODO assign also nuts code? for each level?
 
-			logger.info("Assign land proportion...");
-			GridUtil.assignLandProportion(cells, "LAND_PC", landGeometriesIndex, 2);
+			//TODO too slow - tile that
+			//logger.info("Assign land proportion...");
+			//GridUtil.assignLandProportion(cells, "LAND_PC", landGeometriesIndex, 2);
 
 			//save as GPKG
 			logger.info("Save " + cells.size() + " cells as GPKG...");
