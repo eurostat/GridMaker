4180499042273cea66132a75af70afb11e52a141
jgaffuri <jgaf@protonmail.com>
2020-03-03 18:16:15 +0100

    new tests

--- src/test/java/eu/europa/ec/eurostat/gridmaker/GridMakerJarMainTest.java
+++ src/test/java/eu/europa/ec/eurostat/gridmaker/GridMakerJarMainTest.java
@@ -12,15 +12,24 @@ public class GridMakerJarMainTest extends TestCase {
 		GridMakerJarMain.main(new String[] {"-o", "target/test/out.gpkg"});
 	}
 
-	public void testParams() throws Exception {
-		//TODO test res
-		//TODO test epsg
-		//TODO test gt
-		//TODO test gt
-		//TODO test tol
-		//TODO test inputs + formats
-		//TODO test output formats
-		GridMakerJarMain.main(new String[] {"-res", "500000", "-epsg", "2154", "-gt", "CENTER_POINT", "-o", "target/test/params.shp"});
+	public void testResolution() throws Exception {
+		for(String res : new String[] {"100000","200000","500000","1000000"})
+			GridMakerJarMain.main(new String[] {"-res", res, "-o", "target/test/res_"+res+".gpkg"});
 	}
+
+	public void testEPSG() throws Exception {
+		for(String epsg : new String[] {"3035","2154", "3857"})
+			GridMakerJarMain.main(new String[] {"-epsg", epsg, "-o", "target/test/epsg_"+epsg+".gpkg"});
+	}
+
+	public void testParams() throws Exception {
+		for(String format : new String[] {"shp","gpkg","geojson"})
+			GridMakerJarMain.main(new String[] {"-o", "target/test/outformat"+"."+format});
+	}
+
+	//TODO test gt
+	//TODO test tol
+	//TODO test inputs + formats
+	//TODO test output formats
 
 }
