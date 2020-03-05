4c61963a2b99449081ca8ac0e8f4e60db3025216
jgaffuri <jgaf@protonmail.com>
2020-03-05 10:04:04 +0100

    test cases

---
+++ releases/gridmaker-1.1/tests.bat
@@ -0,0 +1,13 @@
+java -jar GridMaker.jar -o outtests/out.gpkg
+java -jar GridMaker.jar -o outtests/out.geojson
+java -jar GridMaker.jar -o outtests/out.shp
+java -jar GridMaker.jar -res 1000000 -o outtests/res_1000000.gpkg
+java -jar GridMaker.jar -epsg 2154 -o outtests/epsg_2154.gpkg
+java -jar GridMaker.jar -epsg 3857 -o outtests/epsg_3857.gpkg
+java -jar GridMaker.jar -gt SURFACE -o outtests/gt_SURFACE.gpkg
+java -jar GridMaker.jar -gt CENTER_POINT -o outtests/gt_CENTER_POINT.gpkg
+java -jar GridMaker.jar -tol 1000000 -o outtests/tol_1000000.gpkg
+java -jar GridMaker.jar -tol 500000 -o outtests/tol_500000.gpkg
+java -jar GridMaker.jar -i test_data/test_grid_area.geojson -o outtests/i_geojson.gpkg
+java -jar GridMaker.jar -i test_data/test_grid_area.shp -o outtests/i_shp.gpkg
+java -jar GridMaker.jar -i test_data/test_grid_area.gpkg -o outtests/i_gpkg.gpkg
