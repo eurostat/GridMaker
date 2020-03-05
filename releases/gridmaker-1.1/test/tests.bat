4d65cabfa65c75fae288f2c90fc3fae5232666ee
jgaffuri <jgaf@protonmail.com>
2020-03-05 11:32:49 +0100

    path correction

--- releases/gridmaker-1.1/test/tests.bat
+++ releases/gridmaker-1.1/test/tests.bat
@@ -8,6 +8,6 @@ java -jar ../GridMaker.jar -gt SURF -o outtests/gt_SURF.gpkg
 java -jar ../GridMaker.jar -gt CPT -o outtests/gt_CPT.gpkg
 java -jar ../GridMaker.jar -tol 1000000 -o outtests/tol_1000000.gpkg
 java -jar ../GridMaker.jar -tol 500000 -o outtests/tol_500000.gpkg
-java -jar ../GridMaker.jar -i test_data/test_grid_area.geojson -o outtests/i_geojson.gpkg
-java -jar ../GridMaker.jar -i test_data/test_grid_area.shp -o outtests/i_shp.gpkg
-java -jar ../GridMaker.jar -i test_data/test_grid_area.gpkg -o outtests/i_gpkg.gpkg
+java -jar ../GridMaker.jar -i test_grid_area.geojson -o outtests/i_geojson.gpkg
+java -jar ../GridMaker.jar -i test_grid_area.shp -o outtests/i_shp.gpkg
+java -jar ../GridMaker.jar -i test_grid_area.gpkg -o outtests/i_gpkg.gpkg
